b0d0ba537556a96a5f171fe3decdaf5a786b38f1
Ricardo <05rungr@gmail.com>
2020-10-17 00:23:52 -0500

    Test de movimiento, camara y parallax

---
+++ Assets/_scripts/PlayerDummy/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerInput : MonoBehaviour
+{
+    private static PlayerInput SI;
+
+    public bool IsMoving { get; private set; }
+    public bool IsJumping { get; private set; }
+
+    private float _direction;
+
+    [SerializeField, Tooltip("Angulo en hacia el cual se mueve"), Range(-360, 360)]
+    private float cameraAngle = 90;
+
+    [SerializeField, Range(0, 20)] private float velocity;
+
+    private Rigidbody2D _playerRb;
+
+    private void Awake()
+    {
+        SI = SI == null ? this : SI;
+        _playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        IsJumping = false;
+        IsMoving = true;
+    }
+
+    private void Update()
+    {
+        _direction = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        IsMoving = _direction != 0;
+        _playerRb.velocity = IsMoving
+            ? new Vector2(Util.GetVectorFromAngle(cameraAngle).x * velocity * _direction,
+                Util.GetVectorFromAngle(cameraAngle).y * velocity * _direction)
+            : Vector2.zero;
+    }
+}
---
+++ Assets/_scripts/PlayerDummy/PlayerStats.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    //Use getter y setter en lugar de props para que unicamente reste o sume de uno en uno
+    [SerializeField, Range(0, 10)] private int initialLife;
+    private int _currentLife;
+    private int _currentCollectable;
+
+
+    //Llamar en otras clases sin referenciar
+    public static PlayerStats SI;
+
+    private void Awake()
+    {
+        _currentLife = initialLife;
+    }
+
+    /// <summary>
+    /// Resta o suma la vida del player
+    /// </summary>
+    /// <param name="lostLife">True si resta vida, false si suma</param>
+    public void ChangeLife(bool lostLife)
+    {
+        _currentLife = lostLife ? _currentLife-- : _currentLife++;
+    }
+
+    public int GetCurrentLife()
+    {
+        return _currentLife;
+    }
+
+    public int GetCurrentCollectable()
+    {
+        return _currentCollectable;
+    }
+
+    public void WinCollectable()
+    {
+        _currentCollectable++;
+    }
+}
