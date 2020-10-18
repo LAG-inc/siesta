a76080b919df72cfed0bb1dca8df25e832972efe
Daniel Luque <danielluque14@gmail.com>
2020-10-18 20:39:12 +0200

    Sistema de spawn y obstacle

---
+++ Assets/_scripts/Obstacles/ScriptableObstacle.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Obstacle", menuName = "ScriptableObjects/Obstacle", order = 1)]
+public class ScriptableObstacle : ScriptableObject
+{
+    public string obstacleName;
+    public Color32 color;
+}
---
+++ Assets/_scripts/Obstacles/SpawnArea.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+    public GameObject obstacleBase;
+    public int maxObstacles = 10;
+
+    private Queue<GameObject> _queue;
+
+    void Awake()
+    {
+        _queue = new Queue<GameObject>(maxObstacles);
+        PopulateQueue();
+    }
+
+    void PopulateQueue()
+    {
+        for (int i = 0; i < maxObstacles; i++)
+        {
+            GameObject newObstacle = Instantiate(obstacleBase);
+            newObstacle.SetActive(false);
+            _queue.Enqueue(newObstacle);
+        }
+    }
+
+    void SpawnFromQueue(SpawnPoint spawnPoint)
+    {
+        GameObject obstacle = _queue.Dequeue();
+        obstacle.transform.SetParent(spawnPoint.transform);
+        obstacle.transform.position = spawnPoint.transform.position;
+        SetObstacleValues(obstacle, spawnPoint.obstacleValues);
+        obstacle.SetActive(true);
+    }
+
+    void ReturnToQueue(GameObject obstacle)
+    {
+        obstacle.SetActive(false);
+        _queue.Enqueue(obstacle);
+    }
+
+    void SetObstacleValues(GameObject obstacle, ScriptableObstacle obstacleValues)
+    {
+        SpriteRenderer spRenderer =obstacle.GetComponent<SpriteRenderer>();
+        spRenderer.color = obstacleValues.color;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("SpawnPoint"))
+        {
+            SpawnFromQueue(other.gameObject.GetComponent<SpawnPoint>());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            ReturnToQueue(other.gameObject);
+        }
+    }
+}
